87e9ee453437dc57214e3bb7618b10f17d992678
dorllen <dorllen@github.com>
2020-09-23 01:09:44 +0800

    add: sisc_core 辅助工具

---
+++ util/sisc_core/inst_helper.py
@@ -0,0 +1,28 @@
+import re
+from core.sisc_core.mocker import Int, Mem
+
+
+class InstHelper(object):
+
+    @staticmethod
+    def ubfx(*args):
+        assert len(args) == 3, args
+        arg1, arg2, arg3 = args
+        assert isinstance(arg2, int) and isinstance(arg3, int)
+        bin_list = list(bin(arg1).replace("0b", ""))
+        bin_list.reverse()
+        bin_values = bin_list[arg2:arg2 + arg3]
+        bin_values.reverse()
+        bin_value = "".join(bin_values) or "0"
+        return Int("0b" + bin_value, 2)
+
+    @staticmethod
+    def rev(arg):
+        # rev: 0x000000006eb740dc -> 0x00000000dc40b76e 字节反转
+        bstr = bin(arg).replace("0b", "")
+        while len(bstr) % 8 != 0:
+            bstr = "0" + bstr
+        barr = re.findall("[01]{8}", bstr)
+        barr = [Int("0b" + i, 2) for i in barr]
+        barr.reverse()
+        return Mem.merge_arr_to_int(barr)
