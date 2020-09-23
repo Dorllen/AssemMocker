ada580e238775b7647fa12bd2378665ba70b9049
dorllen <dorllen@github.com>
2020-09-24 01:42:47 +0800

    add: 新增test bd_encrypt

--- simple/dy/bd_encrypt_demo.py
+++ simple/dy/bd_encrypt_demo.py
@@ -1,5 +1,5 @@
 from core.sisc_core.mocker import *
-from util.sisc_core.inst_helper import InstHelper
+from util.sisc_core.inst_helper import InstHelper as IH
 
 def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
     _x0 = x0
@@ -8,7 +8,7 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
 
     w8, w9, w10, w11 = Mem.mem_read(x1, 4) # *(x1)
 
-    w8, w9, w10, w11 = InstHelper.rev(w8), InstHelper.rev(w9), InstHelper.rev(w10), InstHelper.rev(w11)
+    w8, w9, w10, w11 = IH.rev(w8), IH.rev(w9), IH.rev(w10), IH.rev(w11)
 
     w12, w13 = Mem.mem_read(x0, 2) # *(x0)
 
@@ -34,9 +34,9 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
     w14 = 0x1ce
     x12 = x0 + 0x18 # 表示 x0 + 0x18 地址
     # w24, w26, w27, w28 已知
-    # x16内容： /Users/pengdongneng/PycharmProjects/secrets/pythonDemo/ios/Nt_ARM/Nt_LLDB/tmp/lldb_mem_0x10c6d9cb0.txt
+    # x16内容： lldb_mem_0x10c6d9cb0.txt
     # x25 = 0x..d8f8. 且读这块内存至少是 0x2000
-    # "/Users/pengdongneng/PycharmProjects/secrets/pythonDemo/ios/Nt_ARM/Nt_LLDB/tmp/lldb_mem_0x10cf1acc8.txt"
+    # "lldb_mem_0x10cf1acc8.txt"
     x25 = Mem.read_file(x25_f_path) #
     while True:
         #loc_100369364
@@ -44,11 +44,11 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         # lldb_mem_0x10c6d9cb0.txt $x16
         # lldb_mem_0x10cf1acc8.txt $x25
         w6 = x25.w[w8 << 2]
-        w8 = InstHelper.ubfx(w26, 16, 8)
+        w8 = IH.ubfx(w26, 16, 8)
         # 0x10cf1b0c8 $x10
         x10 = x25 + 0x400 # 0x..dcf8 都是计算x19后的结果，以x25为基准偏移，+偏移0x400
         w22 = x10.w[w8 << 2]
-        w8 = InstHelper.ubfx(w28, 0x8, 0x8)
+        w8 = IH.ubfx(w28, 0x8, 0x8)
 
         x30 = x10 + 0x400 # 0x..e0f8
         w0 = x30.w[w8 << 2]
@@ -62,9 +62,9 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         w0, w22 = Mem.mem_read(x12 - 8, 2) # *(x12 - 8) # 刚好 0x18 - 0x8 -> 0x10，因为前参数取的是0 ~ 0x10。而在这个循环总共处理x0 + 0x10 ~ 0x10 + 0x30的数据。
         w6 = w17 ^ w0
         w17 = x25.w[w2 << 2]
-        w0 = InstHelper.ubfx(w28, 0x10, 8)
+        w0 = IH.ubfx(w28, 0x10, 8)
         w0 = x10.w[w0 << 2]
-        w2 = InstHelper.ubfx(w24, 8, 8)
+        w2 = IH.ubfx(w24, 8, 8)
         w2 = x30.w[w2 << 2]
         w17 = w0 ^ w17
         w17 = w17 ^ w2
@@ -73,11 +73,11 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         w17 = w17 ^ w0
         w0 = w28 >> 0x18
         w0 = x25.w[w0 << 2]
-        w2 = InstHelper.ubfx(w24, 0x10, 8)
+        w2 = IH.ubfx(w24, 0x10, 8)
         w2 = x10.w[w2 << 2]
         w22 = w17 ^ w22
         w17 = w2 ^ w0
-        w0 = InstHelper.ubfx(w27, 8, 8)
+        w0 = IH.ubfx(w27, 8, 8)
         w0 = x30.w[w0 << 2]
         w17 = w17 ^ w0
         w0 = w26 & 0xFF
@@ -85,13 +85,13 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         w2 = w24 >> 0x18
         w2 = x25.w[w2 << 2]
         w17 = w17 ^ w0
-        w0 = InstHelper.ubfx(w27, 0x10, 8)
+        w0 = IH.ubfx(w27, 0x10, 8)
         w10 = x10.w[w0 << 2]
 
         w0, w25 = Mem.mem_read(x12, 2)
         w24 = w17 ^ w0
         w10 = w10 ^ w2
-        w17 = InstHelper.ubfx(w26, 8, 8)
+        w17 = IH.ubfx(w26, 8, 8)
         w17 = x30.w[w17 << 2]
         w10 = w10 ^ w17
         w17 = w28 & 0xFF
@@ -106,10 +106,10 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
 
             x11 = x25 + 0x1000 #0x1000 = 0x400 * 4;  Mem.read_file("") # ldp [x10] # ida:0x100369720. x11 = 0x..e8f8
             w13 = x11.w[w26 << 2]
-            w14 = InstHelper.ubfx(w22, 0x10, 8)
+            w14 = IH.ubfx(w22, 0x10, 8)
             x12 = x11 + 0x400
             w14 = x12.w[w14 << 2]
-            w15 = InstHelper.ubfx(w24, 8, 8)
+            w15 = IH.ubfx(w24, 8, 8)
 
             x16 = x11 + 0x400
             w15 = x16.w[w15 << 2]
@@ -117,21 +117,21 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
             w17 = x17.w[sp_0xa0_0x90 << 2]
             w0 = w22 > 0x18
             w0 = x11.w[w0 << 2]
-            w1 = InstHelper.ubfx(w24, 0x10, 8)
+            w1 = IH.ubfx(w24, 0x10, 8)
             w1 = x12.w[w1 << 2]
-            w2 = InstHelper.ubfx(w25, 8, 8)
+            w2 = IH.ubfx(w25, 8, 8)
             w2 = x16.w[w2 << 2]
             w3 = w24 >> 0x18
             w3 = x11.w[w3 << 2]
-            w4 = InstHelper.ubfx(w24, 0x10, 8)
+            w4 = IH.ubfx(w24, 0x10, 8)
             w4 = x12.w[w4 << 2]
-            w5 = InstHelper.ubfx(w6, 8, 8)
+            w5 = IH.ubfx(w6, 8, 8)
             w5 = x16.w[w5 << 2]
             w7 = w25 >> 0x18
             w11 = x11.w[w7 << 2]
-            w7 = InstHelper.ubfx(w6, 0x10, 8)
+            w7 = IH.ubfx(w6, 0x10, 8)
             w12 = x12.w[w7 << 2]
-            w7 = InstHelper.ubfx(w22, 8, 8)
+            w7 = IH.ubfx(w22, 8, 8)
             w16 = x16.w[w7 << 2]
 
             x9 = x16 + 0x400
@@ -200,10 +200,10 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         # x10 是个固定值
         x17 = x25 # 实际上是一样的
         w2 = x25.w[w26 << 2]
-        w26 = InstHelper.ubfx(w22, 0x10, 8)
+        w26 = IH.ubfx(w22, 0x10, 8)
         x10 = x17 + 0x400
         w0 = x10.w[w26 << 2]
-        w26 = InstHelper.ubfx(w24, 8, 8)
+        w26 = IH.ubfx(w24, 8, 8)
         w26 = x30.w[w26 << 2]
         w27 = w25 & 0xFF
         w27 = x8.w[w27 << 2]
@@ -215,10 +215,10 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         w27 = w0 ^ w2
         w0 = w22 >> 0x18
         w0 = x17.w[w0 << 2]
-        w2 = InstHelper.ubfx(w24, 0x10, 8)
+        w2 = IH.ubfx(w24, 0x10, 8)
         w2 = x10.w[w2 << 2]
         w0 = w2 ^ w0
-        w2 = InstHelper.ubfx(w25, 8, 8)
+        w2 = IH.ubfx(w25, 8, 8)
         w2 = x30.w[w2 << 2]
         w0 = w0 ^ w2
         w2 = w6 & 0xFF
@@ -227,10 +227,10 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         w26 = w0 ^ w26
         w0 = w24 >> 0x18
         w0 = x17.w[w0 << 2]
-        w2 = InstHelper.ubfx(w25, 0x10, 8)
+        w2 = IH.ubfx(w25, 0x10, 8)
         w2 = x10.w[w2 << 2]
         w0 = w2 ^ w0
-        w2 = InstHelper.ubfx(w6, 8, 8)
+        w2 = IH.ubfx(w6, 8, 8)
         w2 = x30.w[w2 << 2]
         w0 = w0 ^ w2
         w2 = w22 & 0xFF
@@ -241,10 +241,10 @@ def sub_100369264(x0:Mem, x1:Mem, x2:Mem, x25_f_path):
         w28 = w0 ^ w2
         w0 = w25 >> 0x18
         w17 = x17.w[w0 << 2]
-        w0 = InstHelper.ubfx(w6, 0x10, 8)
+        w0 = IH.ubfx(w6, 0x10, 8)
         w10 = x10.w[w0 << 2]
         w10 = w10 ^ w17
-        w17 = InstHelper.ubfx(w22, 8, 8)
+        w17 = IH.ubfx(w22, 8, 8)
         w17 = x30.w[w17 << 2]
         w10 = w10 ^ w17
         w17 = w24 & 0xFF
@@ -260,7 +260,18 @@ if __name__ == '__main__':
     lldb_file_path = "simple/dy/lldb_result"
     x0_path = os.path.join(lldb_file_path, "lldb_mem_0x16f7ee7c0.txt")
     x1_path = os.path.join(lldb_file_path, "lldb_mem_0x16f7ee9a4.txt")
-    x25_f_path = os.path.join(lldb_file_path, "lldb_mem_0x10985acc8.txt")
     x0, x1 = Mem.read_file(x0_path), Mem.read_file(x1_path)
-    value = sub_100369264(x0, x1, x1, x25_f_path)
-    print(value, Mem.merge_arr_to_int(value.to_list()))
+
+    x25_f_path = os.path.join(lldb_file_path, "lldb_mem_0x10985acc8.txt")
+    # 总结：x25是固定内容
+    # x25 = Mem.read_file(os.path.join(lldb_file_path, "lldb_mem_0x10cf1acc8.txt"))
+    # x25_ = Mem.read_file(x25_f_path)
+    # lx25 = x25.to_list()
+    # lx25_ = x25_.to_list()
+    # print(lx25)
+    # print(lx25_)
+    # lx25len = min(len(lx25), len(lx25_))
+    # print(lx25_[:lx25len] == lx25[:lx25len])
+
+    # value = sub_100369264(x0, x1, x1, x25_f_path)
+    # print(value, Mem.merge_arr_to_int(value.to_list()))
